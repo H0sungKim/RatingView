a304e398c566c078836581ac228493998fdd15c2
Hosung.Kim <hosungkim5108@gmail.com>
2025-06-05 15:40:36 +0900

    basic implementation

---
+++ Sources/RatingView/RatingItemView.swift
@@ -0,0 +1,79 @@
+//
+//  StarView.swift
+//  PresentationLayer
+//
+//  Created by 김호성 on 2025.03.31.
+//
+
+import UIKit
+
+class RatingItemView: UIView {
+    
+    let imageView: UIImageView = UIImageView()
+    
+    var accentColor: UIColor? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    var baseColor: UIColor? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    var fill: Float? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    init(image: UIImage) {
+        super.init(frame: .zero)
+        
+        initializeView(image: image)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        initializeView()
+    }
+    
+    private func initializeView(image: UIImage? = nil) {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = image
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        mask = imageView
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let accentColor = accentColor, let baseColor = baseColor, let fill = fill else { return }
+        let width = rect.width
+        let height = rect.height
+        
+        let backgroundRect = CGRect(x: 0, y: 0, width: width, height: height)
+        let backgroundPath = UIBezierPath(rect: backgroundRect)
+        baseColor.setFill()
+        backgroundPath.fill()
+        
+        let fillRect = CGRect(x: 0, y: 0, width: CGFloat(fill)*width, height: height)
+        let fillPath = UIBezierPath(rect: fillRect)
+        accentColor.setFill()
+        fillPath.fill()
+    }
+}
---
+++ Sources/RatingView/RatingView.swift
@@ -0,0 +1,181 @@
+//
+//  RatingView.swift
+//  RatingView
+//
+//  Created by 김호성 on 2025.06.04.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable public class RatingView: UIView {
+    
+    public weak var delegate: RatingViewDelegate?
+    
+    private let stackView: UIStackView = UIStackView()
+    private var ratingItemViews: [RatingItemView] = []
+    
+    private var hapticGenerator: UIImpactFeedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+    
+    public enum Interval: Int {
+        case one = 1
+        case half = 2
+    }
+    public var interval: Interval = .half
+    
+    @IBInspectable public var maximumRating: Int = 5 {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable public var isAnimationEnabled: Bool = true
+    @IBInspectable public var isHapticEnabled: Bool = true
+    
+    @IBInspectable public var symbolImage: UIImage = UIImage(systemName: "star.fill")!
+    
+    @IBInspectable public var accentColor: UIColor? {
+        didSet {
+            didSetAccentColor()
+        }
+    }
+    private func didSetAccentColor() {
+        ratingItemViews.forEach({ ratingItemView in
+            ratingItemView.accentColor = accentColor
+        })
+    }
+    
+    @IBInspectable public var baseColor: UIColor? {
+        didSet {
+            didSetBaseColor()
+        }
+    }
+    private func didSetBaseColor() {
+        ratingItemViews.forEach({ ratingItemView in
+            ratingItemView.baseColor = baseColor
+        })
+    }
+    
+    public var rating: Float = 0 {
+        didSet {
+            didSetRating()
+        }
+    }
+    private func didSetRating() {
+        ratingItemViews.enumerated().forEach({ index, ratingItemView in
+            ratingItemView.fill = max(0, min(rating-Float(index), 1))
+        })
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        updateView()
+        configureStackView()
+        configureGestureRecognizer()
+    }
+    
+    
+    private func updateView() {
+        ratingItemViews = (0..<maximumRating).map({ _ in RatingItemView(image: symbolImage) })
+        stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        ratingItemViews.forEach({ ratingItemView in
+            stackView.addArrangedSubview(ratingItemView)
+        })
+        didSetBaseColor()
+        didSetAccentColor()
+        didSetRating()
+    }
+    
+    private func configureStackView() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    private func animate(rating: Float) {
+        guard isAnimationEnabled else { return }
+        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+            guard let self else { return }
+            ratingItemViews.forEach({ ratingItemView in
+                ratingItemView.transform = CGAffineTransform(scaleX: 1, y: 1)
+            })
+            if 0 < rating && rating <= Float(maximumRating) {
+                ratingItemViews[Int(ceil(rating))-1].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            }
+        })
+    }
+    
+    private func generateHaptic() {
+        guard isHapticEnabled else { return }
+        hapticGenerator.impactOccurred()
+    }
+    
+    // MARK: - Gesture
+    @objc private func panGestureHandler(recognizer: UIPanGestureRecognizer) {
+        gestureHandler(recognizer: recognizer)
+    }
+    
+    @objc private func tapGestureHandler(recognizer: UITapGestureRecognizer) {
+        gestureHandler(recognizer: recognizer)
+    }
+    
+    private func configureGestureRecognizer() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler(recognizer: )))
+        panGestureRecognizer.delegate = self
+        addGestureRecognizer(panGestureRecognizer)
+        
+        let tapGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(tapGestureHandler(recognizer: )))
+        tapGestureRecognizer.minimumPressDuration = 0
+        tapGestureRecognizer.delegate = self
+        addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    private func gestureHandler(recognizer: UIGestureRecognizer) {
+        let discretizedRating: Float = Float(Int(recognizer.location(in: self).x / self.bounds.width * CGFloat(maximumRating) * CGFloat(interval.rawValue))+1) / Float(interval.rawValue)
+        let validRangeStar: Float = max(0, min(discretizedRating, Float(maximumRating)))
+        switch recognizer.state {
+        case .began:
+            generateHaptic()
+            animate(rating: discretizedRating)
+            delegate?.touchDown?(validRangeStar)
+        case .changed:
+            guard validRangeStar != rating else { return }
+            generateHaptic()
+            animate(rating: discretizedRating)
+        case .ended:
+            delegate?.touchUp?(validRangeStar)
+            animate(rating: 0)
+        default:
+            break
+        }
+        delegate?.valueChanged?(validRangeStar)
+        rating = validRangeStar
+    }
+}
+
+extension RatingView: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return gestureRecognizers?.contains(otherGestureRecognizer) ?? false
+    }
+}
+
+@MainActor
+@objc public protocol RatingViewDelegate: AnyObject {
+    @objc optional func valueChanged(_ value: Float)
+    @objc optional func touchDown(_ value: Float)
+    @objc optional func touchUp(_ value: Float)
+}
+
